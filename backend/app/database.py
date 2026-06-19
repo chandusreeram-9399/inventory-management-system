55fab5b5bdd999f3cfee5b0895c3d86da2c043a7
chandusreeram-9399 <sekharchandra9399@gmail.com>
2026-06-20 00:01:34 +0530

    removed the hardcoded url

--- backend/app/database.py
+++ backend/app/database.py
@@ -11,8 +11,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
 DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://neondb_owner:npg_52TcDNuXnUmy@ep-young-forest-aduoz1ie-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+    "DATABASE_URL"
 )
 
 if DATABASE_URL.startswith("sqlite"):
